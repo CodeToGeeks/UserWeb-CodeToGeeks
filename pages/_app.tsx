d62135eae70f1325d8ec271e4dca493a52953759
WagdySamih <WagdySamih787@gmail.com>
2022-06-02 22:59:31 +0200

    Init Commit

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+console.log('x')
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
