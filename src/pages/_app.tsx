47b5eebc82aebf62e6766f3d29788e531de57796
WagdySamih <WagdySamih787@gmail.com>
2022-06-05 01:09:25 +0200

    Added basic file structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import '../../styles/globals.css'
+import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 
-console.log('x')
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
