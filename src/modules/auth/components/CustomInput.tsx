9a13bef79073425bae7c3dde2c5d1406075f5e6a
WagdySamih <WagdySamih787@gmail.com>
2022-07-07 20:17:25 +0200

    Added custom input reusable component

---
+++ src/modules/auth/components/CustomInput.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styles from '../styles/CustomInput.module.scss'
+
+type CustomInputProps = {
+  id: string
+  type: string
+  placeholder: string
+  onChange: (e: React.FormEvent<HTMLInputElement>) => void
+  value: string
+  disabled?: boolean
+  onFocus?: () => void
+  onBlur?: () => void
+  label?: string
+  labelClassName?: string
+}
+
+const CustomInput = ({
+  id,
+  type,
+  placeholder,
+  onChange,
+  value,
+  disabled,
+  onFocus,
+  onBlur,
+  label,
+  labelClassName,
+}: CustomInputProps) => {
+  return (
+    <div className={styles.formControl}>
+      {label && (
+        <label htmlFor={id} className={labelClassName}>
+          {label}
+        </label>
+      )}
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+        className={disabled ? styles.invalid : styles.valid}
+        disabled={disabled}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+    </div>
+  )
+}
+
+export default CustomInput
