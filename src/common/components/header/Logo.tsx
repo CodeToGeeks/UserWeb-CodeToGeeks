1f4982f6f255022804da18b26b13532344583e46
WagdySamih <WagdySamih787@gmail.com>
2022-07-20 02:28:58 +0200

    Added navigation to home page on click on logo

--- src/common/components/header/Logo.tsx
+++ src/common/components/header/Logo.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type AppProps = {
   width?: string
@@ -11,14 +12,16 @@ const Logo = (props: AppProps) => {
   const { styles, ...imageProps } = props
   return (
     <div className={styles || ''}>
-      <Image
-        src="/assets/logo2.png"
-        alt="Code To Geeks"
-        width="100"
-        height="80"
-        priority
-        {...imageProps}
-      />
+      <Link href="/">
+        <Image
+          src="/assets/logo2.png"
+          alt="Code To Geeks"
+          width="100"
+          height="80"
+          priority
+          {...imageProps}
+        />
+      </Link>
     </div>
   )
 }
