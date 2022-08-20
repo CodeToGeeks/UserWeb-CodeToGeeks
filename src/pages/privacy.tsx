60e5405c69662ec0d3d4afa40d6bcf53606dbe2f
WagdySamih <wagdysamih787@gmail.com>
2022-08-20 21:51:35 +0200

    Added privacy policy and terms & conditions pages

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import PrivacyPolicyPage from '@modules/other/pages/PrivacyPolicy'
+
+const PrivacyPolicy: NextPage = () => {
+  return <PrivacyPolicyPage />
+}
+
+export default PrivacyPolicy
