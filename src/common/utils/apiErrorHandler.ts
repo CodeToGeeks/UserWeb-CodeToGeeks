3a4c7af187227cebcc4ca9bde44d033d8513ad2a
Ahmed Ali Ahmed <47031477+AhmedAliAbdAlMowla@users.noreply.github.com>
2022-08-29 01:40:21 +0200

    fix check valid token

--- src/common/utils/apiErrorHandler.ts
+++ src/common/utils/apiErrorHandler.ts
@@ -6,7 +6,10 @@ interface BackendError {
 export const apiErrorHandler = (error: Error | AxiosError): string => {
   if (axios.isAxiosError(error)) {
     const e = error as AxiosError
-    if (e.response?.status === 401) {
+    if (
+      e.response?.status === 401 &&
+      e.response?.config.url === '/auth/token/valid'
+    ) {
       return 'The authentication session has expired. Please sign-in again'
     }
     const backendError = e.response?.data as BackendError
