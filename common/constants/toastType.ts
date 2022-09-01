e57312ff98b36373846c8b179106235b35b842d5
Ahmed Ali Ahmed <47031477+AhmedAliAbdAlMowla@users.noreply.github.com>
2022-09-02 01:00:01 +0200

    Delete common directory

--- common/constants/toastType.ts
+++
@@ -1,6 +0,0 @@
-export enum ToastType {
-  Success = 'success',
-  Error = 'error',
-  Warning = 'warning',
-  Info = 'info',
-}
--- common/models/Author.model.ts
+++
@@ -1,5 +0,0 @@
-export interface Author {
-  _id: string
-  name: string
-  profile_image: string
-}
